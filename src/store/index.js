2039644660b799e7b6aa8ecf09acafeace321afd
Borys Ziukov <ziukoff1985@gmail.com>
2025-12-12 19:30:44 +0200

    Add composeWithDevTools in store/index.js

--- src/store/index.js
+++ src/store/index.js
@@ -1,4 +1,6 @@
 import { createStore } from 'redux';
+import { composeWithDevTools } from '@redux-devtools/extension';
+
 import contactsReducer from './reducers/contactsReducer';
 
-export default createStore(contactsReducer);
+export default createStore(contactsReducer, composeWithDevTools());
