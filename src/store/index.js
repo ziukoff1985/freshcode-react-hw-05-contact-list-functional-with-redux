81b0483da0cb8d179a6fde7b46354bb707f6e5eb
Borys Ziukov <ziukoff1985@gmail.com>
2025-12-28 19:51:10 +0200

    Remove unused comments - before RTK

--- src/store/index.js
+++ src/store/index.js
@@ -1,11 +1,11 @@
 import { createStore } from 'redux';
-import { applyMiddleware } from 'redux'; // метод для додавання middleware
+import { applyMiddleware } from 'redux';
 
-import { composeWithDevTools } from '@redux-devtools/extension'; // для роботи Redux DevTools
-import logger from 'redux-logger'; // для middleware - logger
+import { composeWithDevTools } from '@redux-devtools/extension';
+import logger from 'redux-logger';
 
 import contactsReducer from './reducers/contactsReducer';
 
-const middleware = applyMiddleware(logger); // додавання middleware logger
+const middleware = applyMiddleware(logger);
 
 export default createStore(contactsReducer, composeWithDevTools(middleware));
