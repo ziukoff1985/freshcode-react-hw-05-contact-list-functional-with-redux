30fcfbd28818ddfe008648b967c8c60f9229ec6a
Borys Ziukov <ziukoff1985@gmail.com>
2025-12-29 11:04:49 +0200

    Remove unused comments

--- src/components/ContactItem/ContactItem.jsx
+++ src/components/ContactItem/ContactItem.jsx
@@ -1,25 +1,21 @@
 import { useDispatch, useSelector } from 'react-redux';
 
-import api from '../../api/contactsService';
 import {
     deleteContact,
     setContactForEdit,
-} from '../../store/actions/contactsActions';
+} from '../../store/slices/contactsSlice';
 
 import styles from './ContactItem.module.css';
 
 function ContactItem({ contact }) {
     const dispatch = useDispatch();
 
-    const contactForEdit = useSelector((state) => state.contactForEdit);
+    const contactForEdit = useSelector(
+        (state) => state.contactsList.contactForEdit
+    );
 
-    async function onContactDelete() {
-        try {
-            await api.delete(`/${contact.id}`);
-            dispatch(deleteContact(contact.id));
-        } catch (error) {
-            console.log(error.message);
-        }
+    function onContactDelete() {
+        dispatch(deleteContact(contact.id));
     }
 
     function onContactEdit() {
